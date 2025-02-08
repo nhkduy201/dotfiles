1146e0771bf709bb2e61bb738177aa3304fd0d05
Nguyen Huynh Khanh Duy <43413081+nhkduy201@users.noreply.github.com>
2025-02-08 06:28:52 -0800

    Make windows_google_dns.ps1 one line

--- windows_google_dns.ps1
+++ windows_google_dns.ps1
@@ -1,22 +1,2 @@
-function Set-GoogleDNS {
-    param (
-        [string[]]$IPv4Servers,
-        [string[]]$IPv6Servers
-    )
-
-    # Get all network adapters
-    $networkAdapters = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
-
-    foreach ($adapter in $networkAdapters) {
-        Write-Host "Setting DNS for adapter: $($adapter.Name)"
-        
-        # Set IPv4 DNS
-        Set-DnsClientServerAddress -InterfaceAlias $adapter.Name -ServerAddresses $IPv4Servers
-        
-        # Set IPv6 DNS
-        Set-DnsClientServerAddress -InterfaceAlias $adapter.Name -ServerAddresses $IPv6Servers
-    }
-
-    Write-Host "DNS settings updated successfully."
-}
-Set-GoogleDNS -IPv4Servers @("8.8.8.8", "8.8.4.4") -IPv6Servers @("2001:4860:4860::8888", "2001:4860:4860::8844")
+# Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | ForEach-Object { Set-DnsClientServerAddress -InterfaceAlias $_.Name -ServerAddresses @("1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001") }
+Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | ForEach-Object { Set-DnsClientServerAddress -InterfaceAlias $_.Name -ServerAddresses @("8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844") }
