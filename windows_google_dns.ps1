8cab7013266044b9dc143abaf41e532c0492f064
Nguyen Huynh Khanh Duy <43413081+nhkduy201@users.noreply.github.com>
2025-02-08 06:30:16 -0800

    Add new line make it easier to be executed windows_google_dns.ps1

--- windows_google_dns.ps1
+++ windows_google_dns.ps1
@@ -1,2 +1,3 @@
 # Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | ForEach-Object { Set-DnsClientServerAddress -InterfaceAlias $_.Name -ServerAddresses @("1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001") }
 Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | ForEach-Object { Set-DnsClientServerAddress -InterfaceAlias $_.Name -ServerAddresses @("8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844") }
+
