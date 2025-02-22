453d07f0c8df64c88d8d4a6c4169794883fdd8e9
Nguyen Huynh Khanh Duy <43413081+nhkduy201@users.noreply.github.com>
2025-02-23 00:02:30 +0700

    Create copy_innerText_to_clipboard.js

---
+++ copy_innerText_to_clipboard.js
@@ -0,0 +1,10 @@
+function copyText() {
+  const text = $0.innerText;
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+copyText();
